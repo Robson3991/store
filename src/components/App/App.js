5ced39d61d132ce5b2f157243ba383c2934e5822
Robert Grembowicz <robert.grembowicz@gmail.com>
2021-07-20 20:38:40 +0200

    show items from api

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
+  const [error, setError] = useState(false);
+  const [items, setItems] = useState('');
+
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const getData = await axios.get('/api/cart')
+        setItems(getData.data);
+      } catch(error) {
+        setError(true);
+      }
+    }
+    fetch();
+  }, []);
+
+  if (error) return <div className="container">
+    <p>Nie udało się pobrać danych</p>
+  </div>
+
+  console.log(items);
 
   return (
     <div className="container">
       <h3>Lista produktów</h3>
-      <ul>
-        <li className="row">Patelnia, cena: 89,99zł</li>
+      {
+        items && 
+        <ul>
+        {
+          items.map((item) => (
+            <div className="row product" key={item.pid}>
+              <span>{item.name} - {item.price} zł</span>
+                <button className="button product__button">Dodaj do koszyka</button>
+            </div>
+          ))
+        }
       </ul>
+      }
     </div>
   );
 };
